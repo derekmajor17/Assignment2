066ab601a1557a90650d112b2c4a5648ece4dfe7
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-13 00:15:10 -0500

    Update Project.s

--- Project.s
+++ Project.s
@@ -1,5 +1,10 @@
 .data
   emptyInput:   .asciiz "Input is empty."
-  invalidInput: .asciiz "Invalid base-37 number."
+  invalidInput: .asciiz "Invalid base-27 number."
   longInput:    .asciiz "Input is too long"
   userInput:    .space  512
+  
+  .text
+main:
+  li $v0, 8
+  la $a0, userInput
