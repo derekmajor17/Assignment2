d42c1f99ed110569bfd2550fc86f20638845b26b
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-13 00:21:47 -0500

    Update Project.s

--- Project.s
+++ Project.s
@@ -1,5 +1,5 @@
 .data
-  emptyInput:   .asciiz "Input is empty."
-  invalidInput: .asciiz "Invalid base-27 number."
-  longInput:    .asciiz "Input is too long"
-  userInput:    .space  512
+  emptyInput:   .asciiz "Input is empty."   #Adds string to data
+  invalidInput: .asciiz "Invalid base-27 number."   #Adds string to data
+  longInput:    .asciiz "Input is too long"   #Adds string to data
+  userInput:    .space  512   #User input representing a space
