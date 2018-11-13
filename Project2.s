9ab3305aa510b3412fbaaf837e8b9d7ec9555c56
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-13 00:34:27 -0500

    Update Project2.s

--- Project2.s
+++ Project2.s
@@ -6,13 +6,19 @@
 
 .text
 emptyInputError:    
-  la $a0, emptyInput    #system call code for 
+  la $a0, emptyInput    #system call code for emptyInput
   li $v0, 4   # address of string to print
   syscall
   j exit    #jump to exit program
 
 invalidInputError:
   la $a0, invalidInput #system call code for invalidInput
+  li $v0, 4   # address of string to print
+  syscall
+  j exit    #jump to exit program
+
+LongInputError:
+  la $a0, longInput   #system call code for longInput
   li $v0, 4   # address of string to print
   syscall
   j exit    #jump to exit program
