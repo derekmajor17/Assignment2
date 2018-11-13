69fa19ea01c1bbb3a0ebb6f51b3c38168f25a1bf
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-13 00:27:48 -0500

    Update Project.s

--- Project.s
+++ Project.s
@@ -3,3 +3,10 @@
   invalidInput: .asciiz "Invalid base-27 number."   #Adds string to data
   longInput:    .asciiz "Input is too long"   #Adds string to data
   userInput:    .space  512   #User input representing a space
+
+.text
+emptyInputError:    
+  la $a0, emptyInput    #system call code for printing string
+  li $v0, 4   # address of string to print
+  syscall
+  j exit    #jump to exit program
