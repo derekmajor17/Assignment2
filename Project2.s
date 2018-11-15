d91af8debef1c8a043b0ab861348a1960ff29814
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-15 16:24:31 -0500

    Checks for four characters

--- Project2.s
+++ Project2.s
@@ -29,3 +29,7 @@ main:
   la $a0, userInput   #points to the starting address of user input
      li $v0, 8    # loads userInput in $v0
      syscall
+
+#Check if input is more than 4 characters long
+     lb $t0, 5($a0)                              # sixth byte put into $t0, fifth byte is new line char to enter string
+     bne $zero, $t0, print_more_than_four        # if sixth byte isn't NULL then userInput has more than 4 characters
