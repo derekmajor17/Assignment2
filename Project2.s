c0276384c26edb3cb442b1719e0f94c11ba34cf2
derekmajor17 <43793638+derekmajor17@users.noreply.github.com>
2018-11-15 17:57:12 -0500

    Updating counter and value

--- Project2.s
+++ Project2.s
@@ -31,10 +31,17 @@ main:
      syscall
 
 #Check if input is more than 4 characters long
-     lb $t0, 5($a0)                              # sixth byte put into $t0, fifth byte is new line char to enter string
-     bne $zero, $t0, print_more_than_four        # if sixth byte isn't NULL then userInput has more than 4 characters
+     lb $t0, 5($a0)  # sixth byte put into $t0, fifth byte is new line char to enter string
+     bne $zero, $t0, printMoreThanFour   # if sixth byte isn't NULL then userInput has more than 4 characters
 
-     li $s0, 1                                   # number to multiply 27 with 
-     li $s1, 0                                   # sum number
-     li $s4, 0                                   # loop counter
-     addi $a0, $a0, 4                            # $a0 points to the 5th byte
+     li $s0, 1  # number to multiply 27 with 
+     li $s1, 0  # sum number
+     li $s4, 0  # loop counter
+     addi $a0, $a0, 4  # $a0 points to the fifth byte
+
+loop:
+     li $t5, 4
+     beq $t5, $s4, loopExit
+     addi $s4, $s4, 1  # update value of counter by one
+     addi $a0, $a0, -1 # update value so it points to the address before the previous byte
+
